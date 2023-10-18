fef316529d8c8a249a10c7d0958e574768e0dfbf
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-18 16:55:06 +0300

    Izveidoju veikala pirmo variantu

---
+++ resources/views/products.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Products</title>
+</head>
+<body>
+  <h1>Products</h1>
+  <main>
+    <article>
+      <h2>Doctor's Sausage</h2>
+      <img src="/images/sausage.png" alt="Big juicy sausage" width="200px"/>
+      <p>This is the greatest Sausage. Without a 
+        bite you will miss the 😞
+      </p>
+      <p>1.49 EUR</p>
+    </article>
+  </main>
+</body>
+</html>
